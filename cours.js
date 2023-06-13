38c143919d5c55ad498774aa650e48f9a0937830
Arthur <arthur.philippe21@gmail.com>
2023-06-13 16:21:50 +0200

    Initial commit

---
+++ cours.js
@@ -0,0 +1,37 @@
+// Il existe 3 manières de déclarer une variable en JS : let var const !
+
+/* 
+différence entre let et var :
+- var est une variable globale
+- let est une variable locale
+let est plus sécurisé que var, car il est impossible de modifier une variable let en dehors de son scope
+let ne peut pas être redéclaré alors que var oui
+*/
+
+/*
+switch (expression) {
+    case valeur1:
+        // instructions exécutées quand expression vaut valeur1
+        break;
+    case valeur2:
+        // instructions exécutées quand expression vaut valeur2
+        break;
+    ...
+    default:
+        // instructions exécutées quand aucune des valeurs ne correspond
+        break;
+}
+
+do {
+    // instructions exécutées au moins une fois
+} while (condition)
+
+//Exemple
+var a = 0
+
+do {
+    console.log(a)
+    a++
+} while (a < 10)
+*/
+
